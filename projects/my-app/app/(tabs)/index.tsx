6ec453680bebe5740b9ce395280baea271458f3d
Vinny <vini_masotti@hotmail.com>
2023-03-22 12:43:02 +0000

    hello

---
+++ projects/my-app/app/(tabs)/index.tsx
@@ -0,0 +1,38 @@
+import { StyleSheet } from 'react-native';
+
+import EditScreenInfo from '../../components/EditScreenInfo';
+import { Text, View } from '../../components/Themed';
+
+
+export default function TabOneScreen() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Welcome</Text>
+      <Text style={styles.separator2}>Zip Zap</Text>
+      
+      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
+      <EditScreenInfo path="app/(tabs)/index.tsx" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  title: {
+    fontSize: 50,
+    fontWeight: 'bold',
+  },
+  separator: {
+    marginVertical: 30,
+    height: 1,
+    width: '80%',
+  } ,
+  separator2: {
+    fontSize: 50,
+    fontWeight: 'bold',
+  } ,
+});
