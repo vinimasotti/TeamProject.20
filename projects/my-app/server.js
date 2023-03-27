3b5bd8e854c04af4e5dd49709262ac27a89ae408
Vinny <vini_masotti@hotmail.com>
2023-03-27 23:45:39 +0100

    ejsCommandsAdded

--- projects/my-app/server.js
+++ projects/my-app/server.js
@@ -1,4 +1,27 @@
 const express = require('express')
 const app = express ()
 
+
+app.set('view-engine', 'ejs')
+
+//set up a router
+app.get('/', (req, res) => {
+  res.render('index.ejs', {name: 'Kyle' }) //testing index.ejs
+})
+
+
+//app.get('/login', (req, res) => )
+//}) https://www.youtube.com/watch?v=-RCnNyD0L-s&t=880s
+
+app.get('/login', (req,res) => {
+  res.render('login.ejs')
+})
+
+app.get('/register', (req,res) => {
+  res.render('register.ejs')
+})
+
+app.post('/register',(req, res)=> {
+
+})
 app.listen(3000)
